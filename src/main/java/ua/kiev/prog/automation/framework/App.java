23e3c89975ec3a3e1755ddae0e28f316ff37aab8
Anastasia <anastasiia1127@gmail.com>
2018-07-02 20:46:14 +0300

    changes

--- src/main/java/ua/kiev/prog/automation/framework/App.java
+++ src/main/java/ua/kiev/prog/automation/framework/App.java
@@ -4,8 +4,17 @@ import ua.kiev.prog.automation.framework.product.app.ProgKievUa;
 
 public class App
 {
+    static private boolean _standalone;
+
+    static  public boolean isStandalone(){
+        return _standalone;
+    }
+
     static public void main(String[] args)
     {
+        if (args.length>0 && args[0].equals("standalone"))
+            _standalone=true;
+
         System.setProperty("webdriver.chrome.driver", "C:\\chromedriver_win32\\chromedriver.exe");
         /*ChromeDriver drv = null;
         try {
